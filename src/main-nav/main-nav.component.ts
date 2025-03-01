c7d69c0a92b6a49046544fcc98c4838645cf561b
Matúš Nekoranec <matusnekoranec22@gmail.com>
2025-03-01 10:40:39 +0100

    fixed main nav

--- src/main-nav/main-nav.component.ts
+++ src/main-nav/main-nav.component.ts
@@ -9,48 +9,26 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent implements OnInit {
-  // Tracks whether the left section (e.g., dashboard) should be shown
   showLeftSection = false;
-
-  // Tracks whether the logout button should be displayed
   showLogout: boolean = false;
-
-  // Tracks the state of the hamburger menu
-  isMenuOpen = false;
 
   constructor(private router: Router, public authService: AuthService) { }
 
   ngOnInit(): void {
-    this.router.events.subscribe((event) => {
+    this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        // Show the logout button and left section if the URL contains '/dashboard'
+        // Ak sa URL obsahuje '/dashboard', nastavíme showLogout na true.
         this.showLeftSection = event.url.includes('/dashboard');
         this.showLogout = event.url.includes('/dashboard');
-
-        // Close the hamburger menu when navigating to a new route
-        this.isMenuOpen = false;
       }
     });
   }
 
-  /**
-   * Toggles the state of the hamburger menu.
-   */
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen; // Toggle the menu state
-  }
-
-  /**
-   * Checks if the user is logged in.
-   */
   get isLoggedIn(): boolean {
-    return this.authService.isLoggedIn; // Delegate to the AuthService
+    return this.authService.isLoggedIn;
   }
 
-  /**
-   * Signs out the user by calling the AuthService's signOut method.
-   */
-  signout(): void {
-    this.authService.signOut(); // Delegate to the AuthService
+  signout() {
+    this.authService.signOut()
   }
 }
